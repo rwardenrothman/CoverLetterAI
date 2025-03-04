646c3e1e7b5318e8d794f30ada4b368e1df75e93
rwarden <rwarden@alum.mit.edu>
2025-03-04 08:58:17 -0500

    Set Sphinx theme

--- source/conf.py
+++ source/conf.py
@@ -13,15 +13,41 @@ author = 'Rob Warden-Rothman'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages',
+    'myst_parser',
+    'sphinx.ext.autosectionlabel',
+]
+
+source_suffix = ['.rst', '.md']
 
 templates_path = ['_templates']
 exclude_patterns = []
+autosectionlabel_maxdepth = 3
+autosectionlabel_prefix_document = True
+
+myst_url_schemes = {
+    "http": None,
+    "https": None,
+    "tel": {
+        "url": "tel:{{path}}",
+        "title": "{{path}}",
+    },
+    "mailto": {
+        "url": "mailto:{{path}}",
+        "title": "{{path}}",
+    },
+}
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+html_theme_options = {
+    'body_max_width': 'none'
+}
